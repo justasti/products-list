2cd4a585129d3ea5092fd63009d01e0303d7f75d
justasti <justas.tirevicius@gmail.com>
2023-09-24 08:02:45 +0300

    configure for deploying

--- server/index.js
+++ server/index.js
@@ -28,6 +28,7 @@ mongoose.connect(
 app.use('/meals', mealsRoutes)
 app.use('/products', productsRoutes)
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000')
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+  console.log('Server is running on ' + PORT)
 })
