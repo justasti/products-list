702a56169519b66723aad92e30b91e0efd07ad32
justasti <justas.tirevicius@gmail.com>
2023-11-11 21:27:45 +0200

    implement copy to clipboard

--- client/src/pages/shopping-cart/index.tsx
+++ client/src/pages/shopping-cart/index.tsx
@@ -4,11 +4,21 @@ import { selectShoppingCart } from '../../state/shopping-cart/selectors'
 export const ShoppingCart = () => {
   const shoppingCartItems = useAppSelector(selectShoppingCart)
 
+  const copyToClipboard = () => {
+    const parsedList = shoppingCartItems.map((item) => `${item.name} - ${item.amount}`).join('\n')
+    navigator.clipboard.writeText(parsedList)
+  }
+
   if (!shoppingCartItems.length) return <h1>Shopping cart is empty!</h1>
 
-  return shoppingCartItems.map((item) => (
-    <div>
-      {item.name} - {item.amount}
-    </div>
-  ))
+  return (
+    <>
+      {shoppingCartItems.map((item) => (
+        <div key={item.id}>
+          {item.name} - {item.amount}
+        </div>
+      ))}
+      <button onClick={copyToClipboard}>Copy to clipboard</button>
+    </>
+  )
 }
