21e97ad6aeb2382f87dd82e03bc19fe3e146ab86
justasti <justas.tirevicius@gmail.com>
2023-11-11 20:06:44 +0200

    setup redux-persist for keeping shopping cart state in localstorage

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,15 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from 'react-redux'
+import { persistStore } from 'redux-persist'
+import { PersistGate } from 'redux-persist/integration/react'
 import App from './app/App.tsx'
 import { reduxStore } from './state/store.ts'
 import './styles.css'
+
+let persistor = persistStore(reduxStore)
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={reduxStore}>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 )
--- client/src/state/store.ts
+++ client/src/state/store.ts
@@ -1,18 +1,35 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import { FLUSH, PAUSE, PERSIST, PURGE, REGISTER, REHYDRATE, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 import { mealsSlice } from './meals/slice'
 import { mealsApi } from './mealsApi/api'
 import { productsSlice } from './products/slice'
 import { productsApi } from './productsApi/api'
 
+const rootReducer = combineReducers({
+  [mealsSlice.name]: mealsSlice.reducer,
+  [mealsApi.reducerPath]: mealsApi.reducer,
+  [productsSlice.name]: productsSlice.reducer,
+  [productsApi.reducerPath]: productsApi.reducer,
+})
+
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+  blacklist: [mealsSlice.name, mealsApi.reducerPath, productsSlice.name, productsApi.reducerPath],
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
 export const reduxStore = configureStore({
-  reducer: {
-    [mealsSlice.name]: mealsSlice.reducer,
-    [mealsApi.reducerPath]: mealsApi.reducer,
-    [productsSlice.name]: productsSlice.reducer,
-    [productsApi.reducerPath]: productsApi.reducer,
-  },
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(mealsApi.middleware, productsApi.middleware),
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(mealsApi.middleware, productsApi.middleware),
 })
 
 export type RootState = ReturnType<typeof reduxStore.getState>
