01aafd5cd4a2d6c850847a6b41b6be1e297f15b9
justasti <justas.tirevicius@gmail.com>
2023-11-12 17:22:29 +0200

    handle clicking outside of product to close input

--- client/src/components/products/ProductCard/index.tsx
+++ client/src/components/products/ProductCard/index.tsx
@@ -1,5 +1,5 @@
 import { AddShoppingCartOutlined } from '@mui/icons-material'
-import { FC, useRef, useState } from 'react'
+import { FC, useEffect, useRef, useState } from 'react'
 import { useAppDispatch } from '../../../state/hooks'
 import { Product } from '../../../state/models'
 import { addToCart } from '../../../state/shopping-cart/slice'
@@ -13,6 +13,8 @@ export const ProductCard: FC<Props> = ({ product }) => {
   const dispatch = useAppDispatch()
   const productAmountRef = useRef<HTMLInputElement>()
   const cardRef = useRef<HTMLDivElement>()
+  const addButtonRef = useRef<HTMLButtonElement>()
+  const svgButtonRef = useRef<SVGSVGElement>()
   const [isAddButtonClicked, setIsAddButtonClicked] = useState(false)
 
   const handleSubmit = (e: React.SyntheticEvent) => {
@@ -22,6 +24,24 @@ export const ProductCard: FC<Props> = ({ product }) => {
       setIsAddButtonClicked(false)
     } else setIsAddButtonClicked(true)
   }
+
+  useEffect(() => {
+    const handleDocumentClick = (event: MouseEvent) => {
+      if (
+        !cardRef.current.contains(event.target as Node) &&
+        event.target !== addButtonRef.current &&
+        event.target !== svgButtonRef.current
+      ) {
+        setIsAddButtonClicked(false)
+      }
+    }
+
+    document.addEventListener('click', handleDocumentClick)
+
+    return () => {
+      document.removeEventListener('click', handleDocumentClick)
+    }
+  }, [])
 
   return (
     <StyledProductCard $isInputHidden={!isAddButtonClicked} ref={cardRef}>
@@ -35,8 +55,8 @@ export const ProductCard: FC<Props> = ({ product }) => {
             productAmountRef.current.value = e.target.value
           }}
         />
-        <button>
-          <AddShoppingCartOutlined color='action' />
+        <button ref={addButtonRef}>
+          <AddShoppingCartOutlined ref={svgButtonRef} color='action' />
         </button>
       </form>
     </StyledProductCard>
