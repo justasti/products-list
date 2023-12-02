3626b8d58df5023d162d4521fbd8fa8456074289
justasti <justas.tirevicius@gmail.com>
2023-12-02 17:23:35 +0200

    fix pwa

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,9 +1,44 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import { VitePWA } from 'vite-plugin-pwa'
-import manifest from './public/manifest.json'
+
+const manifest = {
+  registerType: 'prompt',
+  includeAssests: ['favicon.ico', 'apple-touc-icon.png', 'masked-icon.svg'],
+  manifest: {
+    name: 'Shopping List',
+    short_name: 'shoplist',
+    description: 'App to create shopping lists easily',
+    icons: [
+      {
+        src: '/android-chrome-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+        purpose: 'favicon',
+      },
+      {
+        src: '/android-chrome-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'favicon',
+      },
+      {
+        src: '/apple-touch-icon.png',
+        sizes: '180x180',
+        type: 'image/png',
+        purpose: 'apple touch icon',
+      },
+    ],
+    theme_color: '#171717',
+    background_color: '#f0e7db',
+    display: 'standalone',
+    scope: '/',
+    start_url: '/',
+    orientation: 'portrait',
+  },
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA(manifest)],
+  plugins: [react(), VitePWA(manifest as any)],
 })
